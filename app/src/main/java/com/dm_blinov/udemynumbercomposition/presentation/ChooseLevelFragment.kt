a6f11b85638e90595500fad8616f96b5a201900b
Dimapoit <dimapoit88@gmail.com>
2022-02-11 01:47:56 +0200

    presentation

---
+++ app/src/main/java/com/dm_blinov/udemynumbercomposition/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,20 @@
+package com.dm_blinov.udemynumbercomposition.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dm_blinov.udemynumbercomposition.R
+
+
+class ChooseLevelFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    }
+}
